94248e50ed80edb26499840209c9c10c3e803f04
iscemmanuelitd <84645982+iscemmanuelitd@users.noreply.github.com>
2024-01-24 17:44:32 -0600

    Actualizar clientes.js
    se creo servicio nuevo cliente

--- routes/clientes.js
+++ routes/clientes.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const mongo  = require('mongodb').MongoClient
+const client;
 
 /*  SistemaControlClientes       phNFEFlBguEoMdqb   */
 router.post('/newID',async function(req, res, next) {
-        const client = await mongo.connect('mongodb+srv://SistemaControlClientes:phNFEFlBguEoMdqb@cluster0.82r1d65.mongodb.net/');
+         client = await mongo.connect('mongodb+srv://SistemaControlClientes:phNFEFlBguEoMdqb@cluster0.82r1d65.mongodb.net/');
         var coll = client.db('db723').collection('clientes')
         var r =  await coll.countDocuments()
         console.log(r)
@@ -12,5 +13,13 @@ router.post('/newID',async function(req, res, next) {
 });
 
 
+
+router.post('/nuevo', async function(req, res, next){
+     let coll = cliente.db('db723').collection('clientes')
+   let _data = req.body
+_data['_id'] = await coll.countDocuments()+1
+   let r = coll.insertOne(_data)
+   res.send({estatus:true,mensaje:r})
+})
 
 module.exports = router;

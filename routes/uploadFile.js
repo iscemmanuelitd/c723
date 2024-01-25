40f7c940b76391860ef2091e83ae48242ead210c
iscemmanuelitd <84645982+iscemmanuelitd@users.noreply.github.com>
2024-01-25 08:47:38 -0600

    Actualizar uploadFile.js
    Servicio upload ya sube más de un imagen al mismo tiempo

--- routes/uploadFile.js
+++ routes/uploadFile.js
@@ -8,24 +8,21 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
   let cliente = "0001"
   var form = new formidable.IncomingForm();
- 
+   exec(`IF NOT EXIST  uploadFile\\INE\\${cliente}     mkdir uploadFile\\INE\\${cliente}`)
+    
   form.parse(req, function (err, fields, files) {
     let a = files.misFiles
-    a.forEach(e=>{console.log(e.filepath+" "+e.newFilename+" "+e.originalFilename)})
-
-
-/*
-    var oldpath = files.misFiles[0].filepath;
-    var newFile =  files.misFiles[0].newFilename
-    exec(`mkdir uploadFile\\INE\\${cliente}`)
-    
-    var newpath = `uploadFile\\INE\\${newFile}\\` + files.misFiles[0].originalFilename;
-    fs.rename(oldpath, newpath, function (err) {
-      if (err) res.write({estatus:false,mensaje:`Ocurrio un erro al subir la imagen: ${err}`})
-      else res.write({estatus:true,mensaje:`La imagen se guardo correctamente`});
-      res.end();
+    a.forEach(e=>{
+           var newpath = `uploadFile\\INE\\${cliente}\\${e.originalFilename}`
+ 
+    fs.rename(e.filepath, newpath, function (err) {
+      if (err){res.write({estatus:false,mensaje:`Ocurrio un erro al subir la imagen: ${err}`})
+res.end()}
     });
-*/
-  })
+
+  }); // foreach
+
+ res.write({estatus:true,mensaje:`Las imagenes se guardaron correctamente`});
+      res.end();
 });
 module.exports = router;

e2753dd0d83fc18d59cb273b004a53184985386d
iscemmanuelitd <84645982+iscemmanuelitd@users.noreply.github.com>
2024-01-26 08:33:53 -0600

    Actualizar users.js
    La autenticación se hizo directamente con conexión a BD

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,44 @@
-.
+var express = require('express'); 
+ var router = express.Router(); 
+ const mongoClient  = require('mongodb').MongoClient 
+ const msgError = {estatus:false,mensaje:"Fallo la conexion a la BD o el servidor de Servicios Web se encuentra apagado"} 
+ let client=null 
+ /*  SistemaControlClientes       phNFEFlBguEoMdqb   */ 
+  
+  
+ 
+  
+ router.post('/nuevo', async function(req, res, next){ 
+       let _data = req.query 
+       try{ 
+             console.log(_data) 
+             var _db = client.db("db723") 
+             var col = _db.collection("clientes") 
+             _data['_id'] = await col.countDocuments()+1 
+             let r = await col.insertOne(_data) 
+             res.send({estatus:true,mensaje:"Cliente agregado satisfactoriamente"}) 
+       }catch(errMsg){ 
+             console.log(errMsg) 
+             res.send(msgError) 
+       } 
+ }) 
+  
+  
+ router.post('/autenticar', async function(req, res, next){ 
+       let _data = req.query
+       try{ 
+             var _db = client.db("db723") 
+             var col = _db.collection("usuarios") 
+             let r = await col.findOne(_data) 
+             console.log(r._id)             
+             if(r._id === undefined) res.send({estatus:false,mensaje:"El usuario o la contraseña no coinciden con ningúno de nuestros usuarios Verifique sus datos o comuníquese con el administrador del sistema"})    
+             else res.send({estatus:true,datos:r,mensaje:`Usuarios identificado satisfactoriamente.`}) 
+       }catch(err){ 
+             console.log(err) 
+             res.send(msgError) 
+       } 
+ }) 
+  
+  
+  
+ module.exports = router;
